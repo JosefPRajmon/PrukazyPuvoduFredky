e9286b1afa667c5c43b306ad6acabadc53906c55
rajmo <rajmon.josef1999@seznam.cz>
2022-04-01 14:09:15 +0200

    j

---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="cz">
+<head>
+    <meta charset="utf-8">
+    <title>Rozcestník/login</title>
+</head>
+<body>
+    <h1>Rozcestník / Login</h1>
+    <form>
+        <input type="button" name="LoginBTN" value="Přihlášení">
+        <input type="button" name="registeringBTN" value="Registrace">
+    </form>
+</body>
+</html>
